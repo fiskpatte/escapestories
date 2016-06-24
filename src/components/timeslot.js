8bc87787d6e0699ea173ba59f317c8a42be1ae55
Abebe Hailu <abebeh@kth.se>
2016-06-24 16:04:55 +0200

    Added some layout on buttons

--- src/components/timeslot.js
+++ src/components/timeslot.js
@@ -3,13 +3,20 @@ import React from 'react';
 class Timeslot extends React.Component {
 
   render() {
+    var bookButtonStyle = {
+        backgroundColor: '#4CAF50',
+        border: 'none',
+        color: 'white',
+        webkitBorderRadius: '12px',
+        padding: '5px 10px'
+    }
     return (
       <div>
       {typeof this.props.slot == 'undefined' ? "" :
         this.props.slot.breakin == "open" ||
           this.props.slot.manuscript == "open" ||
           this.props.slot.coverup == "open"
-          ? <button>Boka</button>
+          ? <button style={bookButtonStyle}>Boka</button>
           : "Fullt"
          }
       </div>

627796aed88986abb993528e1b8ca773dd10a2b7
Abebe Hailu <abebeh@kth.se>
2016-08-01 22:29:52 +0200

    WIP About Us - Added som text and some images

--- src/components/contactinfo.js
+++ src/components/contactinfo.js
@@ -9,7 +9,41 @@ class Contactinfo extends React.Component {
     render() {
         return (
             <div className="content">
-              <p>Ring oss fan inte! rinka </p>
+                <div className="contactinfo-description">
+                    <h1>Vilka är vi?</h1>
+                    <p>Fantastiska rum är endast en del av pusselbiten. God kundservice är nyckeln till en oförglömlig upplevelse, och våra GameMasters är här så att vi kan garantera detta.</p>
+                </div>
+
+                <div className="row image-row">
+                    <div className="col-sm-6 col-md-3">
+                        <img src="melody.jpg" className="employee-image"/>
+                        <div className="employee-info">
+                            <h4>Miss Melody</h4>
+                            <p>aka ClueQueen</p>
+                        </div>
+                    </div>
+                    <div className="col-sm-6 col-md-3">
+                        <img src="fredrik.jpg" className="employee-image"/>
+                        <div className="employee-info">
+                            <h4>Fredrik</h4>
+                            <p>aka Mister Fox</p>
+                        </div>
+                    </div>
+                    <div className="col-sm-6 col-md-3">
+                        <img src="meron.jpg" className="employee-image"/>
+                        <div className="employee-info">
+                            <h4>Meron</h4>
+                            <p>aka The Time Keeper</p>
+                        </div>
+                    </div>
+                    <div className="col-sm-6 col-md-3">
+                        <img src="patrik.jpg" className="employee-image"/>
+                        <div className="employee-info">
+                            <h4>Patrik</h4>
+                            <p>aka Babyface</p>
+                        </div>
+                    </div>
+                </div>
             </div>
           );
     }
